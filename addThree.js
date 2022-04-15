0380af8e59ba782c40d2c1bfd88b0b8ad5a3d748
Esi Ogagaoghene <=esi.ogagaoghene@yahoo.com>
2022-04-15 22:12:03 +0000

    First Commit - Added addTwo and addThree functions

---
+++ addThree.js
@@ -0,0 +1,9 @@
+function addThree(first, second, third) {
+	return (first + second + third);
+}
+
+let first  = 90;
+let second = 100;
+let third  = 300;
+
+console.log(addThree(first, second, third));
---
+++ addTwo.js
@@ -0,0 +1,8 @@
+const addTwo = (first, second) => {
+	return(first + second);
+}
+
+let first  = 20;
+let second = 30;
+
+console.log(addtwo(first, second));
